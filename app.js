89e29854e157e9f184c9c3e158ff23985f72a2cb
Abdo-Rabea <76593694+Abdo-Rabea@users.noreply.github.com>
2026-02-10 17:36:40 +0200

    feat: serving static files

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ app.use((req, res, next) => {
   next(); // to call the next middleware function in the stack, if we don't call next() the request will be stuck and the server will not respond to the client.
 });
 app.use(morgan('dev'));
+
+// serving static files
+app.use(express.static(`${__dirname}/public`)); // this is a middleware function that serves static files from the specified directory. in this case, it serves files from the 'public' directory. when a request is made for a file that exists in the 'public' directory, it will be served directly without going through any other route handlers or middleware functions. this is useful for serving images, CSS files, JavaScript files, and other static assets that are needed for the frontend of the application.
+
 // order of using middleware functions is important, if we use app.use(express.json()) after defining the routes, the req.body will be undefined in the route handlers because the middleware function will not be executed before the route handlers. so we need to use app.use(express.json()) before defining the routes to ensure that the request body is parsed and available in the route handlers.
 
 app.use('/api/v1/tours', tourRouter);
