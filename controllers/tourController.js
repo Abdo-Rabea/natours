eb06dc4bc7bd687be4aa2ca00c19511ac68a4302
Abdo-Rabea <76593694+Abdo-Rabea@users.noreply.github.com>
2026-02-24 17:37:52 +0200

    feat: refactor tour crud operations from fs into mongoose model

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,8 +1,4 @@
-const fs = require('fs');
 const Tour = require('../models/tourModel');
-
-const TOURS_FILE = `${__dirname}/../dev-data/data/tours-simple.json`;
-const tours = JSON.parse(fs.readFileSync(TOURS_FILE, 'utf-8'));
 
 // mongoo will hanlde id check for us
 // const checkID = (req, res, next, val) => {
@@ -20,112 +16,110 @@ const tours = JSON.parse(fs.readFileSync(TOURS_FILE, 'utf-8'));
 //   next();
 // };
 
-const checkBody = (req, res, next) => {
-  if (!req.body || !req.body.name || !req.body.price) {
-    return res.status(400).json({
-      status: 'fail',
-      message: 'Missing name or price',
-    });
-  }
-  next();
-};
+// this check is done with our mongoose chema validation
+// const checkBody = (req, res, next) => {
+//   if (!req.body || !req.body.name || !req.body.price) {
+//     return res.status(400).json({
+//       status: 'fail',
+//       message: 'Missing name or price',
+//     });
+//   }
+//   next();
+// };
 
 // 2. route handlers
-const getAllTours = (req, res) => {
-  res.status(200).json({
-    // it is Jsend specification which is a standard for structuring JSON responses in APIs. it has three main properties: status, data, and message. status is a string that indicates the status of the response, it can be 'success', 'fail', or 'error'. data is an object that contains the actual data being returned in the response. message is a string that provides additional information about the response, it is usually used in case of errors to provide more details about what went wrong.
-    status: 'success',
-    requestTime: req.requestTime,
-    results: tours.length, // not part of Jsend specification but it is a common practice to include the number of results in the response when returning a list of items. Johnas opinion
-    data: {
-      tours,
-    },
-  });
+const getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+
+    res.status(200).json({
+      // it is Jsend specification which is a standard for structuring JSON responses in APIs. it has three main properties: status, data, and message. status is a string that indicates the status of the response, it can be 'success', 'fail', or 'error'. data is an object that contains the actual data being returned in the response. message is a string that provides additional information about the response, it is usually used in case of errors to provide more details about what went wrong.
+      status: 'success',
+      requestTime: req.requestTime,
+      results: tours.length, // not part of Jsend specification but it is a common practice to include the number of results in the response when returning a list of items. Johnas opinion
+      data: {
+        tours,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
-const createTour = (req, res) => {
-  // create new tour
-  const id = tours[tours.length - 1].id + 1;
-  // eslint-disable-next-line prefer-object-spread
-  const newTour = Object.assign({ id }, req.body);
-  tours.push(newTour);
-  // write new tour to file
-  fs.writeFile(TOURS_FILE, JSON.stringify(tours), (err) => {
-    if (err) {
-      res.status(500).json({
-        status: 'error',
-        message: 'Failed to save the new tour',
-      });
-    } else {
-      res.status(201).json({
-        status: 'success',
-        data: {
-          tour: newTour,
-        },
-      });
-    }
-  });
+const createTour = async (req, res) => {
+  const newTour = await Tour.create(req.body);
+  try {
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: 'error',
+      message: err,
+    });
+  }
 };
 
-const getTour = (req, res) => {
+const getTour = async (req, res) => {
   // console.log(req.params);
-  const tour = tours[req.tourIndex]; // <-- from me
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour,
-    },
-  });
+  try {
+    const tour = await Tour.findById(req.params.id);
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'tour not found',
+    });
+  }
 };
 
-const updateTour = (req, res) => {
-  const tour = tours[req.tourIndex]; // <-- from me
-
-  // update the tour
-  // the next line will update tours[id] object
-  Object.assign(tour, req.body);
-
-  // write the updated tour to file
-  fs.writeFile(TOURS_FILE, JSON.stringify(tours), (err) => {
-    if (err) {
-      res.status(500).json({
-        status: 'error',
-        message: 'Failed to update the tour',
-      });
-    } else {
-      res.status(200).json({
-        status: 'success',
-        data: {
-          tour,
-        },
-      });
-    }
-  });
+const updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: 'error',
+      message: 'Failed to update the tour',
+    });
+  }
 };
 
-const deleteTour = (req, res) => {
-  const { tourIndex } = req;
-
-  tours.splice(tourIndex, 1);
-  fs.writeFile(TOURS_FILE, JSON.stringify(tours), (err) => {
-    if (err) {
-      res.status(500).json({
-        status: 'error',
-        message: 'Failed to delete the tour',
-      });
-    } else {
-      res.status(204).json({
-        // 204 status code means no content, it is used when the server successfully processed the request but is not returning any content. it is commonly used in delete requests to indicate that the resource has been successfully deleted and there is no content to return in the response.
-        status: 'success',
-        data: null,
-      });
-    }
-  });
+const deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: 'error',
+      message: 'Failed to delete the tour',
+    });
+  }
 };
 
 module.exports = {
-  checkBody,
   getAllTours,
   createTour,
   getTour,
