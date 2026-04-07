c32a95ee7e0a7c10cf55ed16a1940cfd1aa4a5d2
Abdo-Rabea <76593694+Abdo-Rabea@users.noreply.github.com>
2026-04-07 10:05:58 +0200

    feat: fill all tours overview page with dynamic data

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,5 +1,11 @@
-const getOverview = (req, res) => {
-  res.status(200).render('overview', { title: 'All tours overview' });
+const Tour = require('../models/tourModel');
+
+const getOverview = async (req, res) => {
+  // 1) Get tours data
+  const tours = await Tour.find();
+  // 2) build template
+  // 3) render that template using tour data from 1)
+  res.status(200).render('overview', { title: 'All tours overview', tours });
 };
 
 const getTour = (req, res) => {
