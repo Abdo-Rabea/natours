bb2348ecd1602365814f7bd3c281e6a909629116
Abdo-Rabea <76593694+Abdo-Rabea@users.noreply.github.com>
2026-04-08 07:46:51 +0200

    feat: create tour details dynamic page

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,16 +1,22 @@
 const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
 
-const getOverview = async (req, res) => {
+const getOverview = catchAsync(async (req, res) => {
   // 1) Get tours data
   const tours = await Tour.find();
   // 2) build template
   // 3) render that template using tour data from 1)
   res.status(200).render('overview', { title: 'All tours overview', tours });
-};
+});
 
-const getTour = (req, res) => {
-  res.status(200).render('tour', { title: 'The Forest Hiker' });
-};
+const getTour = catchAsync(async (req, res) => {
+  const { slug } = req.params;
+  const tour = await Tour.findOne({ slug }).populate({
+    path: 'reviews',
+    select: 'review rating user',
+  });
+  res.status(200).render('tour', { title: `${tour.name} Tour`, tour });
+});
 
 module.exports = {
   getOverview,
