cca8e433e1dea7d4f9f03370309ddd29705871e3
Abdo-Rabea <76593694+Abdo-Rabea@users.noreply.github.com>
2026-04-14 11:43:21 +0200

    feat: update user password

--- public/js/index.js
+++ public/js/index.js
@@ -3,7 +3,7 @@
 import { login } from './login';
 import { logout } from './logout';
 import { displayMap } from './mapbox';
-import { updateUserData } from './updateSettings';
+import { updateUserData, updateUserPassword } from './updateSettings';
 
 // this will be the entry point for our JavaScript code
 // Index.js is about getting data from user interface and delegating actions for other modules for example (get data from login form and call the login functionality)
@@ -12,6 +12,7 @@ const form = document.querySelector('form.form.form--login');
 const map = document.getElementById('map');
 const logOutBtn = document.querySelector('.nav__el--logout');
 const userDataForm = document.querySelector('.form-user-data');
+const userPasswordForm = document.querySelector('.form-user-password');
 if (map) {
   const locations = JSON.parse(map.dataset.locations);
   displayMap(locations);
@@ -42,5 +43,35 @@ if (userDataForm) {
     const email = document.getElementById('email').value;
 
     await updateUserData(name, email);
+  });
+}
+
+if (userPasswordForm) {
+  userPasswordForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const savePasswordBtn = document.querySelector('.btn--save-password');
+    const currentPasswordEl = document.getElementById('password-current');
+    const passwordEl = document.getElementById('password');
+    const confirmPasswordEl = document.getElementById('password-confirm');
+
+    const currentPassword = currentPasswordEl.value;
+    const password = passwordEl.value;
+    const confirmPassword = confirmPasswordEl.value;
+
+    savePasswordBtn.textContent = 'Updating...';
+    savePasswordBtn.disabled = true;
+
+    await updateUserPassword({
+      currentPassword,
+      password,
+      confirmPassword,
+    });
+
+    savePasswordBtn.textContent = 'save password';
+    savePasswordBtn.disabled = false;
+
+    currentPasswordEl.value = '';
+    passwordEl.value = '';
+    confirmPasswordEl.value = '';
   });
 }
--- public/js/updateSettings.js
+++ public/js/updateSettings.js
@@ -19,3 +19,18 @@ export async function updateUserData(name, email) {
     showAlert('error', err.response.data.message);
   }
 }
+export async function updateUserPassword(data) {
+  try {
+    const res = await axios({
+      method: 'PATCH',
+      url: 'http://localhost:3000/api/v1/users/update-password',
+      data,
+    });
+    if (res.data.status === 'success') {
+      showAlert('success', 'Password updated successfully!');
+    }
+  } catch (err) {
+    console.log(err);
+    showAlert('error', err.response.data.message);
+  }
+}
